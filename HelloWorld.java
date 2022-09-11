eb4a10c6560f08211f650c7c0dc0d2d31d522f89
blipat <blipat@winchendon.org>
2022-09-11 17:13:18 -0400

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,3 +1,13 @@
+/*
+ * Every program in Java is written as a class. 
+ * Java is an object-oriented language and we’ll learn more about classes and objects later. 
+ * Inside the class, there can be a main method that starts the program. 
+ * When you ask the Java run-time to run a class, it will always start execution in the main method. 
+ * 
+ * In Java every open curly brace { must have a matched close curly brace }. 
+ * These are used to start and end class definitions and method definitions.
+ */
+
 public class HelloWorld
 {
    public static void main(String[] args)
