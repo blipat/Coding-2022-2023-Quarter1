19c51ee357182ab4902df0db7f6adb636318f5fe
Bernard Lipat-Silva Jandrey <blipat@winchendon.org>
2022-11-09 08:50:23 -0500

    Update Pet.java

--- Pet.java
+++ Pet.java
@@ -21,14 +21,16 @@ In your Awesome Animal Clinic Pet class from the last exercise:
 
 /**
     Pet class (complete comments)
-    @author
-    @since
+    @author 
+    @since 
 */
 public class Pet
 {
    // keep track of the name, age, weight, type of animal, and breed of the pet
 
    // Write 2 constructors and accessor (get) methods. Use good commenting.
+
+   // complete class definition with set methods
 
    // Don't forget to complete the main method in the PetTesterClass!
 }
