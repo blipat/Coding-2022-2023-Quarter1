e2fab29fd121307bf301f14618ef8785a9b1eda8
Bernard Lipat-Silva Jandrey <blipat@winchendon.org>
2022-11-09 08:48:36 -0500

    added Pet.java and PetTesterClass.java

---
+++ Pet.java
@@ -0,0 +1,34 @@
+/* PART 1
+You’ve been hired to create a software system for the Awesome Animal Clinic! They would like to keep track of their animal patients. Here are some attributes of the pets that they would like to track:
+• Name
+• Age
+• Weight
+• Type (dog, cat, frog, lizard, etc.)
+• Breed
+
+1. Create a class that keeps track of the attributes above for pet records at the animal clinic. Decide what instance variables are needed and their data types. Make sure you use boolean, int, double, and String data types. Make the instance variables private.
+2. Create 2 constructors, one with no parameters and one with many parameters to initialize all the instance variables.
+3. Create Accessor (get) methods for each of the instance variables.
+4. In the main method below, create 3 pet objects and call their constructors and accessor methods to test all of your methods.
+5. Make sure you use good commenting to describe the instance variables and methods
+*/
+
+/* PART 2
+In your Awesome Animal Clinic Pet class from the last exercise:
+1. Add set methods for each of the 5 instance variables. Make sure you use good commenting!
+2. Test each of the set methods in the main method.
+*/
+
+/**
+    Pet class (complete comments)
+    @author
+    @since
+*/
+public class Pet
+{
+   // keep track of the name, age, weight, type of animal, and breed of the pet
+
+   // Write 2 constructors and accessor (get) methods. Use good commenting.
+
+   // Don't forget to complete the main method in the PetTesterClass below!
+}
